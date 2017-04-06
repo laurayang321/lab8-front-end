304b1db726b36a6e38b766fd25de55dbbd0935f2
laurayang321 <laurayang321@gmail.com>
2017-04-05 20:38:34 -0700

    update front end starter template with Jim's change

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -133,4 +133,4 @@ $autoload['language'] = array();
   |
   |	$autoload['model'] = array('first_model' => 'first');
  */
-$autoload['model'] = array('categories','menu');
+$autoload['model'] = array('categories','menu', 'order');
